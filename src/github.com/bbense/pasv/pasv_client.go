d948ea2da2206e0632d652eba6c51a6e7d383e44
Booker C. Bense <bbense@gmail.com>
2013-10-18 14:48:26 -0700

    More wip.

---
+++ src/github.com/bbense/pasv/pasv_client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+	"flag"
+)
+
+// We will need three things from the command line, the status, the service and the message. 
+var (
+	nstatus = flag.Int("status",0,"Status value for nagios service check")
+	nservice = flag.String("checkname","default","Name of service check")
+)
+
+// We should have defaults for the remctl stuff. 
+
+func main() {
+
+	flag.Parse()
+	message := strings.Join(flag.Args()," ")
+
+	fmt.Printf("Sending %s : %d : %s\n",*nservice,*nstatus,message)
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
